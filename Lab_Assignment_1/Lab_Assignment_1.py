f4f1f976f6c8efbe6e1da08a4f94a29c031059d5
JohnDT-MechE <johndtrujillo@arizona.edu>
2025-01-16 22:30:59 -0700

    Completed Activity 1

---
+++ Lab_Assignment_1/Lab_Assignment_1.py
@@ -0,0 +1,10 @@
+# Lab Assignment 1
+# John Dominguez-Trujillo
+
+# Activity 1
+# Adding Two Numbers
+num1 = 5
+num2 = 8
+num_sum = num1 + num2
+output = f'The sum of {num1} and {num2} is {num_sum}'
+print(output)
